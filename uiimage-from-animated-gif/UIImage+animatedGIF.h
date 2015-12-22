cd211c9b95e947c0a09402efb8cbfab062acf031
Chris Cieslak <chris.cieslak@vividseats.com>
2015-12-22 10:11:14 -0600

    Add Swift-compatible annotations to header file

--- uiimage-from-animated-gif/UIImage+animatedGIF.h
+++ uiimage-from-animated-gif/UIImage+animatedGIF.h
@@ -18,7 +18,7 @@
     
     For example, suppose the GIF contains three frames.  Frame 0 has duration 3.  Frame 1 has duration 9.  Frame 2 has duration 15.  I divide each duration by the greatest common denominator of all the durations, which is 3, and add each frame the resulting number of times.  Thus `animation` will contain frame 0 3/3 = 1 time, then frame 1 9/3 = 3 times, then frame 2 15/3 = 5 times.  I set `animation.duration` to (3+9+15)/100 = 0.27 seconds.
 */
-+ (UIImage *)animatedImageWithAnimatedGIFData:(NSData *)theData;
++ (UIImage * _Nullable)animatedImageWithAnimatedGIFData:(NSData * _Nonnull)theData;
 
 /*
         UIImage *image = [UIImage animatedImageWithAnimatedGIFURL:theURL];
@@ -27,6 +27,6 @@
     
     I operate exactly like `+[UIImage animatedImageWithAnimatedGIFData:]`, except that I read the data from `theURL`.  If `theURL` is not a `file:` URL, you probably want to call me on a background thread or GCD queue to avoid blocking the main thread.
 */
-+ (UIImage *)animatedImageWithAnimatedGIFURL:(NSURL *)theURL;
++ (UIImage * _Nullable)animatedImageWithAnimatedGIFURL:(NSURL * _Nonnull)theURL;
 
 @end
